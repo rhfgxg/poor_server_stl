eefaf02b9f825d2f8011ac0587d63d8c1b22a1fa
rhfgxg <m13411806653@163.com>
2024-12-24 16:19:41 +0800

    修改日志文件按日创建新文件 对应修改日志文件夹

--- src/common/logger_manager.cpp
+++ src/common/logger_manager.cpp
@@ -1,5 +1,6 @@
 #include "logger_manager.h"
 #include <ctime>
+#include <spdlog/sinks/daily_file_sink.h>
 
 void LoggerManager::initialize(myproject::ServerType server_type)
 {
@@ -12,26 +13,26 @@ void LoggerManager::initialize(myproject::ServerType server_type)
     console_sink->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%^%l%$] [%s:%#] %v");
 
     // 创建不同分类的文件日志器
-    auto create_file_logger = [&](LogCategory category, const std::string& filename) {
-        auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(log_directory + "/" + filename, true);
+    auto create_file_logger = [&](LogCategory category,const std::string& log_type) {
+        auto file_sink = std::make_shared<spdlog::sinks::daily_file_sink_mt>(log_directory + "/" + log_type + "/log.log",0,0); // 按日期滚动
         file_sink->set_level(spdlog::level::debug);
         file_sink->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%^%l%$] [%s:%#] %v");
 
-        spdlog::sinks_init_list sink_list = { console_sink, file_sink };
-        auto logger = std::make_shared<spdlog::logger>(std::to_string(static_cast<int>(category)), sink_list.begin(), sink_list.end());
+        spdlog::sinks_init_list sink_list = {console_sink,file_sink};
+        auto logger = std::make_shared<spdlog::logger>(std::to_string(static_cast<int>(category)),sink_list.begin(),sink_list.end());
         logger->set_level(spdlog::level::debug);
         loggers[category] = logger;
-        };
+    };
 
-    create_file_logger(LogCategory::STARTUP_SHUTDOWN, "startup_shutdown.log");
-    create_file_logger(LogCategory::REQUESTS_RESPONSES, "requests_responses.log");
-    create_file_logger(LogCategory::ERRORS_EXCEPTIONS, "errors_exceptions.log");
-    create_file_logger(LogCategory::CONNECTION_POOL, "connection_pool.log");
-    create_file_logger(LogCategory::PERFORMANCE_MONITORING, "performance_monitoring.log");
-    create_file_logger(LogCategory::SECURITY, "security.log");
-    create_file_logger(LogCategory::DEBUGGING, "debugging.log");
-    create_file_logger(LogCategory::CONFIGURATION_CHANGES, "configuration_changes.log");
-    create_file_logger(LogCategory::SCHEDULED_TASKS, "scheduled_tasks.log");
+    create_file_logger(LogCategory::STARTUP_SHUTDOWN,"startup_shutdown");
+    create_file_logger(LogCategory::REQUESTS_RESPONSES,"requests_responses");
+    create_file_logger(LogCategory::ERRORS_EXCEPTIONS,"errors_exceptions");
+    create_file_logger(LogCategory::CONNECTION_POOL,"connection_pool");
+    create_file_logger(LogCategory::PERFORMANCE_MONITORING,"performance_monitoring");
+    create_file_logger(LogCategory::SECURITY,"security");
+    create_file_logger(LogCategory::DEBUGGING,"debugging");
+    create_file_logger(LogCategory::CONFIGURATION_CHANGES,"configuration_changes");
+    create_file_logger(LogCategory::SCHEDULED_TASKS,"scheduled_tasks");
 
     // 设置默认日志器
     spdlog::set_default_logger(loggers[LogCategory::DEBUGGING]);
@@ -43,11 +44,10 @@ void LoggerManager::initialize(myproject::ServerType server_type)
 std::shared_ptr<spdlog::logger> LoggerManager::getLogger(LogCategory category)
 {
     auto it = loggers.find(category);
-    if (it != loggers.end())
+    if(it != loggers.end())
     {
         return it->second;
-    }
-    else
+    } else
     {
         // 如果日志器不存在，返回默认日志器
         return spdlog::default_logger();
@@ -57,7 +57,7 @@ std::shared_ptr<spdlog::logger> LoggerManager::getLogger(LogCategory category)
 // 创建日志文件夹
 void LoggerManager::createLogDirectory(myproject::ServerType server_type)
 {
-    std::string server_name = "defult"; // 默认日志文件夹名
+    std::string server_name = "default"; // 默认日志文件夹名
 
     switch(server_type)
     {
@@ -77,17 +77,11 @@ void LoggerManager::createLogDirectory(myproject::ServerType server_type)
     server_name = "logic_server";
     break;
     default:
-    server_name = "defult";
+    server_name = "default";
     break;
     }
 
-    // 获取当前日期
-    std::time_t t = std::time(nullptr);
-    std::tm tm = *std::localtime(&t);
-    char date[11];
-    std::strftime(date, sizeof(date), "%Y/%m/%d", &tm);
-
     // 创建日志文件夹
-    log_directory = "../../logs/" + server_name + "/" + date;
+    log_directory = "../../logs/" + server_name;
     std::filesystem::create_directories(log_directory);
 }
