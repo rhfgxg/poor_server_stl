2e411337ab2b34abc0ce1d5a98c4b60959c36894
rhfgxg <m13411806653@163.com>
2025-02-05 10:56:38 +0800

    添加proto文件 将逻辑服务器的任务进一步细分 战斗服务器battle 匹配服务器matching 商城服务器store

--- tools/debug/protoc_make.cmd
+++ tools/debug/protoc_make.cmd
@@ -10,33 +10,46 @@ set MAKE_OUT=%PROJECT_DIR%\protobuf\src
 set PROTOC=%PROJECT_DIR%\vcpkg_installed\x64-windows\tools\protobuf\protoc.exe
 set GRPC_PLUGIN=%PROJECT_DIR%\vcpkg_installed\x64-windows\tools\grpc\grpc_cpp_plugin.exe
 
-rem 生成共享模块：common：包含共享数据类型，枚举的定义
+
+rem 生成 common 模块：：包含共享数据类型，枚举的定义
 %PROTOC% --proto_path=%PROTO_DIR% --cpp_out=%MAKE_OUT% %PROTO_DIR%\common.proto
 %PROTOC% --proto_path=%PROTO_DIR% --grpc_out=%MAKE_OUT% --plugin=protoc-gen-grpc=%GRPC_PLUGIN% %PROTO_DIR%\common.proto
 
-rem 生成 central 模块：中心模块
+rem 生成 battle 模块：战斗服务器
+%PROTOC% --proto_path=%PROTO_DIR% --cpp_out=%MAKE_OUT% %PROTO_DIR%\server_battle.proto
+%PROTOC% --proto_path=%PROTO_DIR% --grpc_out=%MAKE_OUT% --plugin=protoc-gen-grpc=%GRPC_PLUGIN% %PROTO_DIR%\server_battle.proto
+
+rem 生成 central 模块：中心服务器
 %PROTOC% --proto_path=%PROTO_DIR% --cpp_out=%MAKE_OUT% %PROTO_DIR%\server_central.proto
 %PROTOC% --proto_path=%PROTO_DIR% --grpc_out=%MAKE_OUT% --plugin=protoc-gen-grpc=%GRPC_PLUGIN% %PROTO_DIR%\server_central.proto
 
-rem 生成 db 模块：数据库模块
+rem 生成 db 模块：数据库服务器
 %PROTOC% --proto_path=%PROTO_DIR% --cpp_out=%MAKE_OUT% %PROTO_DIR%\server_db.proto
 %PROTOC% --proto_path=%PROTO_DIR% --grpc_out=%MAKE_OUT% --plugin=protoc-gen-grpc=%GRPC_PLUGIN% %PROTO_DIR%\server_db.proto
 
-rem 生成 file 模块：文件服务器模块
+rem 生成 file 模块：文件服务器
 %PROTOC% --proto_path=%PROTO_DIR% --cpp_out=%MAKE_OUT% %PROTO_DIR%\server_file.proto
 %PROTOC% --proto_path=%PROTO_DIR% --grpc_out=%MAKE_OUT% --plugin=protoc-gen-grpc=%GRPC_PLUGIN% %PROTO_DIR%\server_file.proto
 
-rem 生成 gateway 模块：网关模块
+rem 生成 gateway 模块：网关服务器
 %PROTOC% --proto_path=%PROTO_DIR% --cpp_out=%MAKE_OUT% %PROTO_DIR%\server_gateway.proto
 %PROTOC% --proto_path=%PROTO_DIR% --grpc_out=%MAKE_OUT% --plugin=protoc-gen-grpc=%GRPC_PLUGIN% %PROTO_DIR%\server_gateway.proto
 
-rem 生成 logic 模块：逻辑服务模块
+rem 生成 logic 模块：逻辑服务器
 %PROTOC% --proto_path=%PROTO_DIR% --cpp_out=%MAKE_OUT% %PROTO_DIR%\server_logic.proto
 %PROTOC% --proto_path=%PROTO_DIR% --grpc_out=%MAKE_OUT% --plugin=protoc-gen-grpc=%GRPC_PLUGIN% %PROTO_DIR%\server_logic.proto
 
-rem 生成 login 模块：登录模块
+rem 生成 login 模块：登录服务器
 %PROTOC% --proto_path=%PROTO_DIR% --cpp_out=%MAKE_OUT% %PROTO_DIR%\server_login.proto
 %PROTOC% --proto_path=%PROTO_DIR% --grpc_out=%MAKE_OUT% --plugin=protoc-gen-grpc=%GRPC_PLUGIN% %PROTO_DIR%\server_login.proto
+
+rem 生成 matching 模块：匹配服务器
+%PROTOC% --proto_path=%PROTO_DIR% --cpp_out=%MAKE_OUT% %PROTO_DIR%\server_matching.proto
+%PROTOC% --proto_path=%PROTO_DIR% --grpc_out=%MAKE_OUT% --plugin=protoc-gen-grpc=%GRPC_PLUGIN% %PROTO_DIR%\server_matching.proto
+
+rem 生成 store 模块：商城服务器
+%PROTOC% --proto_path=%PROTO_DIR% --cpp_out=%MAKE_OUT% %PROTO_DIR%\server_store.proto
+%PROTOC% --proto_path=%PROTO_DIR% --grpc_out=%MAKE_OUT% --plugin=protoc-gen-grpc=%GRPC_PLUGIN% %PROTO_DIR%\server_store.proto
 
 
 echo 脚本执行完毕
